5753e1a34e4cf95b70809199739b844499d8e10e
dima-boom <78297357+dima-boom@users.noreply.github.com>
2021-11-21 19:28:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 try:
-	import vk_api, requests, time, threading, psycopg2 
+	import vk_api, requests, time, threading, psycopg2, os
 	from vk_api.longpoll import VkLongPoll, VkEventType
 	from vk_api.utils import get_random_id
 	from vk_api.keyboard import VkKeyboard, VkKeyboardColor
